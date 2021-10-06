22f0cf20e776456cd109c05770e952649cec87f3
VÍCTOR RIPOLL BERENGUER <vicribe@upv.edu.es>
2021-10-06 18:11:08 +0200

    Config firebase

---
+++ database/firebase.js
@@ -0,0 +1,26 @@
+import firebase from "firebase";
+
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBcljfI648ysrCctZtPb8Y-2wsT5LnHC3I",
+    authDomain: "room-for-you-87832.firebaseapp.com",
+    projectId: "room-for-you-87832",
+    storageBucket: "room-for-you-87832.appspot.com",
+    messagingSenderId: "46279018872",
+    appId: "1:46279018872:web:4ee5fec0088ea27f30e08d",
+    measurementId: "G-BWHGN84GL0"
+  };
+
+  
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export default {
+    firebase,
+    db,
+}
