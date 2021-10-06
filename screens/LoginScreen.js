c4048b0d910276db34fa1572122300f71e22886d
VÍCTOR RIPOLL BERENGUER <vicribe@upv.edu.es>
2021-10-06 17:41:44 +0200

    mi primer commit

---
+++ screens/LoginScreen.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import { View, Text, TextInput, Button, ScrollView, StyleSheet } from "react-native";
+
+
+const LoginScreen = () => {
+    const [state, setState] = useState({
+        nombre:"",
+        email:"",
+        telefono:""
+    });
+
+    const handleChangeText = (name, value) => {
+        setState({...state, [name]: value});
+    }
+
+    return (
+        <ScrollView>
+            <View>
+                <Text>
+                    Login
+                </Text>
+            </View>
+            <View style={styles.inputComponent}>
+                <TextInput placeholder="Nombre" 
+                onChangeText={(value => handleChangeText("nombre", value))} />
+            </View>
+            <View style={styles.inputComponent}>
+                <TextInput placeholder="Email"
+                onChangeText={(value => handleChangeText("email", value))} />
+            </View>
+            <View style={styles.inputComponent}>
+                <TextInput placeholder="Telefono"
+                onChangeText={(value => handleChangeText("telefono", value))} />
+            </View>
+            <View>
+                <Button title="Registrarse" onPress={() => console.log(state)}/>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputComponent: {
+        flex:1,
+        padding:0,
+        margin:10,
+        marginBottom:15,
+        borderBottomWidth:1,
+        borderBottomColor:'#cccccc'
+    }
+
+})
+
+export default LoginScreen
