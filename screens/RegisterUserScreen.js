41c2ca5175f0aa7333ac6e034bde1555e07884dd
diegoger222 <runyxp@gmail.com>
2021-10-12 12:30:46 +0200

    calendario

--- screens/RegisterUserScreen.js
+++ screens/RegisterUserScreen.js
@@ -1,12 +1,20 @@
 import React, {useState} from "react";
-import { View, Text, TextInput, Button, ScrollView, StyleSheet } from "react-native";
+import { View, Text, TextInput, Button, ScrollView, StyleSheet} from "react-native";
 import '../clases/usuario'
 //import {anadirusuario} from'../database/pruebas'
-import '../database/pruebas'
+import DatePicker from 'react-datepicker';
+import '../database/pruebas';
 import { getFirestore, collection, getDocs,doc, addDoc } from 'firebase/firestore/lite';
-import firebase from '../database/firebase'
+import firebase from '../database/firebase';
 import { crearpersona } from "../clases/usuario";
 import { anadirusuario } from "../database/pruebas";
+//import 'react-datepicker/dist/react-datepicker.css';
+import DateTimePicker from "@react-native-community/datetimepicker";
+//import {DatePickerIOS} from "react-native@react-native-community/datetimepicker"
+
+
+    
+
 
 const db = firebase.db;
 
@@ -16,24 +24,58 @@ const RegisterUserScreen = () => {
         apellidos:'',
         tags:'',
         fechanacimiento:'',
+       time: new Date("2020","06","24"),
         numerotelefono:'',
         email:'',
         contrasena:''
     });
 
+
+
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+        setState({...state, time: currentDate});
+      };
+
+      const showMode = currentMode => {
+        setShow(true);
+        setMode(currentMode);
+      };
+
+      const showDatepicker = () => {
+        showMode('date');
+      };
+      const showTimepicker = () => {
+        showMode('time');
+      };
+
+
     const handleChangeText = (name, value) => {
         setState({...state, [name]: value});
     }
+    const myDateFunction = (date) => {
+        setState({...state, time: value});
+      
+    }
+
+    
+    
 
     const RegisterUser = async () => {
-        if (state.nombre === '' || state.apellidos === '' || state.tags === '' || state.fechanacimiento === '' || state.numerotelefono === '' || state.email === ''|| state.contrasena === '' ){
+        if (state.nombre === '' || state.apellidos === '' || state.tags === '' || state.time === '' || state.numerotelefono === '' || state.email === ''|| state.contrasena === '' ){
             alert('Por favor rellena los campos')
         } else{
             await addDoc(collection(db,'Usuario'),{
                 Apellidos: state.apellidos,
                 Contrasena: state.contrasena,
                 Email: state.email,
-                Fechanacimiento: Date(1995,11,17),
+                Fechanacimiento: state.time,
                 Nombre: state.nombre,
                 Numerotelefono: state.numerotelefono,
                 tags: state.tags
@@ -41,7 +83,8 @@ const RegisterUserScreen = () => {
             alert('Se ha registrado correctamente')
         }
     }
-
+    
+// selected = {this.state.time} onChanged={(value)=>handleChangeText("time" ,value)}
     return (
         <ScrollView>
             <View>
@@ -61,9 +104,24 @@ const RegisterUserScreen = () => {
                 <TextInput placeholder="Tags"
                 onChangeText={(value) => handleChangeText("tags", value)} />
             </View>
-            <View style={styles.inputComponent}>
-                <TextInput placeholder="Fecha Nacimiento" 
-                onChangeText={(value) => handleChangeText("fechanacimiento", value)} />
+            <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+            <View style={styles.container}>
+            {show && (
+                 <DateTimePicker
+                 testID="dateTimePicker"
+                 timeZoneOffsetInMinutes={0}
+                 value={date}
+                 mode={mode}
+                 is24Hour={true}
+                 display="default"
+                 onChange={onChange}
+               />      
+                  )}
             </View>
             <View style={styles.inputComponent}>
                 <TextInput placeholder="Numero Telefono" 
