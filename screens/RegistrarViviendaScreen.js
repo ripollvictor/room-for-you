c3096901253f44ba5a1c00a8c040a3684ab7ebb6
sergiaguilera <sergiaguileramorales@gmail.com>
2021-10-18 17:50:39 +0200

    permisos acceder galeria

--- screens/RegistrarViviendaScreen.js
+++ screens/RegistrarViviendaScreen.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, ScrollView, StyleSheet } from "react-native";
+import * as Permissions from 'expo-permissions'
+import * as ImagePicker from 'expo-image-picker'
+import { View, Text, TextInput, Button, ScrollView, StyleSheet, Alert} from "react-native";
+import {} from "react-native-imagepicker"
+import {Icon, Avatar} from "react-native-elements";
 import '../clases/usuario'
 //import {anadirusuario} from'../database/pruebas'
 import '../database/pruebas'
@@ -7,9 +11,12 @@ import firebase from '../database/firebase'
 import { getFirestore, collection, getDocs,doc, addDoc } from 'firebase/firestore/lite';
 import { crearpersona } from "../clases/usuario";
 import { anadirusuario } from "../database/pruebas";
+import {map, size} from "lodash"
 
 const db = firebase.db;
 
+
+   
 
 const RegistrarViviendaScreen = () => {
     const [state, setState] = useState({
@@ -23,7 +30,129 @@ const RegistrarViviendaScreen = () => {
         puerta: ""
     });
 
-    const handleChangeText = (name, value) => {
+    var img = new Image();
+
+   const selectFile = () => {
+
+        var options = {
+    
+          title: 'Select Image',
+    
+          customButtons: [
+    
+            { 
+    
+              name: 'customOptionKey', 
+    
+              title: 'Choose file from Custom Option' 
+    
+            },
+    
+          ],
+    
+          storageOptions: {
+    
+            skipBackup: true,
+    
+            path: 'images',
+    
+          },
+    
+        };
+    
+        ImagePicker.launchImageLibraryAsync(options, res => {
+            console.log('Response = ', res);
+
+            if (res.didCancel) {
+
+              console.log('User cancelled image picker');
+      
+            } else if (res.error) {
+      
+              console.log('ImagePicker Error: ', res.error);
+      
+            } else if (res.customButton) {
+      
+              console.log('User tapped custom button: ', res.customButton);
+      
+              alert(res.customButton);
+      
+            } else {
+      
+              let source = res;
+      
+              this.setState({
+      
+                resourcePath: source,
+      
+              });
+
+              img.src(source)
+      
+            }
+      
+          });
+    
+      };
+
+    // const selecImagenGaleria = async(array) => {
+    //     const respuesta = {status: false, image: null}
+    //     const resultPermisos = await Permissions.askAsync(Permissions.CAMERA)
+    //     if (resultPermisos.status == "denied"){
+    //         alert("Debes dar acceso a la galería para poder subir imágenes")
+    //         return respuesta
+    //     }
+    //     const result = await ImagePicker.launchImageLibraryAsync({
+    //         allowsEditing: true,
+    //         aspect: array
+    //     })
+    //     if(result.cancelled) {return respuesta}
+    //     respuesta.status = true
+    //     respuesta.image = result.uri
+    //     return respuesta
+    // }
+
+    // const [imagenesSeleccionadas, setImagenesSeleccionadas] = useState([])
+
+    // function SubirImagen ({imagenesSeleccionadas, setImagenesSeleccionadas}) {
+    //     const selecImagen = async() => {
+    //         const respuesta = await selecImagenGaleria([4,4])
+    //         if(respuesta.status == false) {
+    //             alert('No has seleccionado ninguna imagen')
+    //             return
+    //         }
+    //         setImagenesSeleccionadas([...imagenesSeleccionadas, respuesta.image])
+    //     }
+    //     return(
+    //         <ScrollView horizontal>
+    //             {
+    //                 size(imagenesSeleccionadas) < 10 && (
+    //                     <Icon 
+    //                          style={styles.selecionarImagen}
+    //                          tipo="material-community" 
+    //                          name="camera" 
+    //                          onPress={selecImagen}
+    //                          > 
+    //                     </Icon>
+    //                 )
+    //             }
+    //             {
+    //                 map(imagenesSeleccionadas, (imagenPiso, index) => (
+    //                     <Avatar
+    //                         style={styles.selecionarImagen}
+    //                         key={index}
+    //                         source={{uri: imagenPiso}}
+    //                     />
+    //                 )
+    //                 )
+    //             }
+                
+                
+    //         </ScrollView>
+    //     )
+    // }
+
+    function handleChangeText(name, value) {
         setState({ ...state, [name]: value });
     }
 
@@ -82,6 +211,18 @@ const RegistrarViviendaScreen = () => {
                 <TextInput placeholder="Baños"
                     onChangeText={(value => handleChangeText("banos", value))} />
             </View>
+            
+            {/* <SubirImagen
+                setImagenesSeleccionadas={setImagenesSeleccionadas}
+                imagenesSeleccionadas={imagenesSeleccionadas}
+                onPress={SubirImagen}
+            /> */}
+            <View>
+                <Button title="Foto" onPress={() => selectFile()} />
+            </View>
+            <View>
+                
+            </View>
             <View>
                 <Button title="Registrarse" onPress={() => RegisterVivienda()} />
             </View>
@@ -114,7 +255,14 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         borderBottomWidth: 1,
         borderBottomColor: '#cccccc'
+    },
+    selecionarImagen: {
+        height: 70,
+        width: 79,
+        alignItems: "center",
+        justifyContent: "center"
     }
+
 
 })
 
