8427f1b886453bb5cd2c902db4cce783b23d3463
diegoger222 <runyxp@gmail.com>
2021-10-19 10:02:56 +0200

    cosas

--- screens/Login2.js
+++ screens/Login2.js
@@ -4,36 +4,62 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, ScrollView, StyleSheet } from "react-native";
 
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider,signOut } from "firebase/auth";
 const provider = new GoogleAuthProvider();
-const Login2 = () => {
-    
-    const Logueate = () => {
 
-const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+
+
+const Login2 = () => {
+    const [user, setState] = useState({
+    
+    });
+const Logueate = () => {
+
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+           user = result.user;
+           this.setState({user});
+           
+            // ...
+         }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+         });
 };
+
+const logout=() => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+};
+
   return ( 
     <ScrollView>
         <View style={styles.button}>
+                { user 
+                  ?<Text>Hola,{user.displayName}</Text>
+                  :<Text> Porfavor logueate</Text>
+                }
+        </View>
+        <View style={styles.button}>
                 <Button color= '#7733CC' onPress={Logueate} title="Seleccionar Fecha" />
+        </View>
+        <View style={styles.button}>
+                <Button color= '#7733CC' onPress={logout} title="Cerrar Sesion" />
         </View>
     </ScrollView>
 )
