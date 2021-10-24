a0de4d8233f0be3fa77c5ff01d95952dce9a543b
Arturo <45513197+Calisto184AC0@users.noreply.github.com>
2021-10-24 23:35:10 +0200

    autenticación más o menos hecha

--- screens/IniciarSesionScreen.js
+++ screens/IniciarSesionScreen.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, Image, Text } from "react-native"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+ 
 
 import Button from "../components/Button"
 import Input from "../components/Input"
@@ -8,17 +10,39 @@ import { screenStyles } from '../styles/IniciarSesionScreenStyles'
 
 const IniciarSesionScreen = ({navigation}) => {
 
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChangeText = (key, value) => {
+        setCredentials({...credentials, [key]: value})
+    }
+
+
+    // https://firebase.google.com/docs/auth/web/password-auth?hl=es#sign_in_a_user_with_an_email_address_and_password
+    const checkLogIn = () => {
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, credentials.email, credentials.password)
+            .then((userCredential) => {
+                alert('Logeado')
+            })
+            .catch((er) => {
+                alert('Upsi')
+            })
+    }
+
     return(
         <View style={screenStyles.container}>
             <Image style={screenStyles.logo} source={require('../assets/logo.png')} />
             <Text style={screenStyles.mainText}>Introduce tus datos</Text>
 
             {/* AQUI VAN LOS INPUTS */}
-            <Input placeholder='Correo electrónico' style={screenStyles.inputSpace} />
-            <Input inputType='password' placeholder='Contraseña' />
+            <Input onChangeText={(value) => { handleChangeText('email', value) }} placeholder='Correo electrónico' style={screenStyles.inputSpace} />
+            <Input onChangeText={(value) => { handleChangeText('password', value) }} inputType='password' placeholder='Contraseña' />
 
             <View style={screenStyles.btnWrap}>
-                <Button btnType='blue'>Iniciar sesión</Button>
+                <Button btnType='blue' onPress={() => { checkLogIn() }}>Iniciar sesión</Button>
             </View>
 
         </View>
