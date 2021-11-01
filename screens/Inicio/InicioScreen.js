1a25c95bd446744feeb631c6cf525059bb9c19ad
Arturo <45513197+Calisto184AC0@users.noreply.github.com>
2021-11-01 22:12:49 +0100

    solucionado un problemilla con las ventanas

--- screens/Inicio/InicioScreen.js
+++ screens/Inicio/InicioScreen.js
@@ -15,9 +15,9 @@ const InicioScreen = ({navigation}) => {
         <View style={screenStyles.container}>
             <Image style={screenStyles.logo} source={require('../../assets/logo.png')} />
             <Text style={screenStyles.welcomeText}>Bienvenido</Text>
-            <Button onPress={() => { IrPagina('IniciarSesionScreen') }} style={screenStyles.buttonSpace}>Iniciar sesión</Button>
+            <Button onPress={() => { IrPagina('IniciarSesion') }} style={screenStyles.buttonSpace}>Iniciar sesión</Button>
             <Button style={screenStyles.buttonSpace} imgSrc={require('../../assets/google-icon.png')}>Iniciar sesión con Google</Button>
-            <Button onPress={() => { IrPagina('RegistrarUsuarioScreen') }}>Registrarse</Button>
+            <Button onPress={() => { IrPagina('RegistrarUsuario') }}>Registrarse</Button>
         </View>
     )
 
