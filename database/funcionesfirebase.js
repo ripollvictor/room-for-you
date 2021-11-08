667a9db66410951b6b6a7155c10efd06a0f79b3f
diegoger222 <runyxp@gmail.com>
2021-11-08 20:32:28 +0100

    solicitudvivienda

--- database/funcionesfirebase.js
+++ database/funcionesfirebase.js
@@ -1,49 +1,45 @@
-import firebase from '../database/firebase'
+import firebase from './firebase'
 //import { collection, doc,setDoc } from 'firebase/firestore';
 import '../clases/usuario'
 //import firebase from '../database/firebase'
 import { getFirestore, collection, getDocs,doc ,addDoc, query,where} from 'firebase/firestore/lite';
 import { getAuth } from "firebase/auth";
+import { connectStorageEmulator } from '@firebase/storage';
+
 
 const db = firebase.db;
+export function anadirSolicitud(idVivienda){
+    const email = emailUsuario();
+    conseguirIdUsuario(email,idVivienda);
+}
 
-export async function anadirSolicitud(idVivienda){
- //(async ()=>{
-     const email = emailUsuario();
-     const idUse = idUsuario(email);
+async function solicitudaBase(iduse,idVivienda){
      const docRef = await addDoc(collection(db,'Solicitud'),{
          Estado:0,
-         Fecha:Date.now(),
-         id_usuario: idUse,
-         id_vivienda: idVivienda
-         
+         Fecha: new Date(),
+         id_usuario: iduse,
+         id_vivienda: idVivienda        
      })
-
- //})
-
 }
+
+async function conseguirIdUsuario(email,idVivienda) {
+    const q = query(collection(db, "Usuario"), where("Email", "==", email));
+    const querySnapshot = await getDocs(q);
+    const iduse = null;  
+    querySnapshot.forEach((doc) => {
+           solicitudaBase(doc.id,idVivienda);        
+      });
+}
+
 //para conseguir el email actual del usuario
 function emailUsuario(){
     const auth = getAuth();
     const user = auth.currentUser;
-    const email = null;
+    const email1 = null;
     if(user!=null){
-         email = user.email;
+         return user.email;
     }else{
         console.log("Upsi2.0");
     }
-    return email;
-}
-
-//conseguir el id de la base de datos  del usuario actual
-function idUsuario(email){
-    (async ()=>{
-    const q = query(collection(db, "Usuario"), where("Email", "==", email));
-
-const querySnapshot = await getDocs(q);
-querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-     return doc.id;
-});
-})
+    return email1;
 }
