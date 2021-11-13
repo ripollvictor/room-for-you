b4874a843cae0bdd504ac2a847959960c540b4f0
diegoger222 <runyxp@gmail.com>
2021-11-13 18:08:43 +0100

    cosas

--- screens/ModificarVivienda/ModificarVivienda.js
+++ screens/ModificarVivienda/ModificarVivienda.js
@@ -9,16 +9,19 @@ import Button from "../../components/Button/Button"
 import {getViviendaconid , listaVivienda}from "../../database/funcionesfirebase"
 
 const ModificarVivienda = ({navigation,vivienda}) => {
+    
+    
     const [Vivienda, setCredentials] = useState({
-        Banos: vivienda.Banos,
-        Direccion: vivienda.Direccion,
-        EscaleraPisoPuerta: vivienda.EscaleraPisoPuerta,
-        FechaRegistro: vivienda.FechaRegistro,
-        Imagenes: vivienda.Imagenes,
-        MetrosCuadrados: vivienda.MetrosCuadrados,
-        NumHabitaciones: vivienda.NumHabitaciones,
-        Ubicacion: vivienda.Ubicacion,
-        id_usuario: vivienda.id_usuario
+        Banos: vivienda.data().Banos,
+        Direccion: vivienda.data().Direccion,
+        EscaleraPisoPuerta: vivienda.data().EscaleraPisoPuerta,
+        FechaRegistro: vivienda.data().FechaRegistro,
+        Imagenes: vivienda.data().Imagenes,
+        MetrosCuadrados: vivienda.data().MetrosCuadrados,
+        NumHabitaciones: vivienda.data().NumHabitaciones,
+        Ubicacion: vivienda.data().Ubicacion,
+        id_usuario: vivienda.data().id_usuario,
+        id_vivienda: vivienda.id
     })
     const handleChangeText = (name, value) => {
         setState({...Vivienda, [name]: value});
