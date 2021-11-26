064ee02827f338f0e1cccecb006655493011806e
Arturo <45513197+Calisto184AC0@users.noreply.github.com>
2021-11-26 10:23:30 +0100

    de nada

--- src/screens/RegistrarUsuario/RegistrarUsuarioScreen_1.js
+++ src/screens/RegistrarUsuario/RegistrarUsuarioScreen_1.js
@@ -1,116 +1,303 @@
 import { screenStyles } from './styles'
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, Button, ScrollView } from "react-native";
-import { getFirestore, collection, getDocs, doc ,addDoc, query,where,deleteDoc,setDoc,getDoc,updateDoc} from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, doc, addDoc, query, where, deleteDoc, setDoc, getDoc, updateDoc } from 'firebase/firestore/lite';
 import firebase from '../../../old/database/firebase';
-import { getAuth, signInWithPopup, GoogleAuthProvider,signOut,createUserWithEmailAndPassword,updateProfile, update} from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, createUserWithEmailAndPassword, updateProfile, update } from "firebase/auth";
 import { async } from '@firebase/util';
 
 const db = firebase.db;
 
 const RegistrarUsuario1Screen = () => {
     var [state, setState] = useState({
-        nombre:'',
-        apellidos:'',
-        tags:'',
-        fechaNacimiento:'',
-        time: new Date("2020","02","22"),
-        numerotelefono:'',
-        email:'',
-        contrasena:''
+        nombre: '',
+        apellidos: '',
+        tags: '',
+        fechaNacimiento: '',
+        time: new Date("2020", "02", "22"),
+        numerotelefono: '',
+        email: '',
+        contrasena: ''
     });
 
+    const [pruebas, setPruebas] = useState([
+        {
+            nombre: '',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: '',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege1@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: '',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege2@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '',
+            email: 'samuelvege3@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: '',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege4@gmail.com',
+            contrasena: ''
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege5@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: '123Sam123uel123',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege6@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Xx_Samuel_xX',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege7@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: '123Di123az123',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege8@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Xx_Diaz_xX',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege9@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: 'Esto626No',
+            email: 'samuelvege10@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '123456789098765432177013',
+            email: 'samuelvege11@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '177013',
+            email: 'samuelvege12@gmail.com',
+            contrasena: 'S177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege13@gmail.com',
+            contrasena: '177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege14@gmail.com',
+            contrasena: 'samuel'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege15@gmail.com',
+            contrasena: 's177013'
+        },
+        {
+            nombre: 'Samuel',
+            apellidos: 'Diaz',
+            tags: 'Deportista',
+            fechaNacimiento: '',
+            time: new Date("2020", "02", "22"),
+            numerotelefono: '626802895',
+            email: 'samuelvege16@gmail.com',
+            contrasena: 'S123'
+        }
+    ])
+
+    let [index, setIndex] = useState(-1)
+
+    useEffect(() => {
+        if (index >= 0 && index < pruebas.length - 1)
+            RegisterUser()
+    }, [state])
+
+    useEffect(() => {
+        if (index >= 0)
+            setState(pruebas[index])
+            console.log(index)
+    }, [index])
+
+    const ejecutarPrueba = () => {
+        setIndex(index + 1)
+    }
+
     const RegisterUser = async () => {
-        if (state.nombre === '' || state.apellidos === '' || state.tags === '' ||  state.numerotelefono === '' || state.email === ''|| state.contrasena === '' ){
+        if (state.nombre === '' || state.apellidos === '' || state.tags === '' || state.numerotelefono === '' || state.email === '' || state.contrasena === '') {
             alert('Por favor rellena los campos')
-        } else{
+        } else {
 
-            try{
-             if((await compruebaEmail(state.email)).length != 0){
-                 alert('El correo ya esta en uso')
-             }else{
-            Registroconemail(state.email,state.contrasena);     
-            
-            await addDoc(collection(db,'Usuario'),{
-                Apellidos: state.apellidos,
-                Contrasena: state.contrasena,
-                Email: state.email,
-                FechaNacimiento: state.time,
-                Nombre: state.nombre,
-                NumeroTelefono: state.numerotelefono,
-                tags: state.tags
-            });
+            try {
+                if ((await compruebaEmail(state.email)).length != 0) {
+                    alert('El correo ya esta en uso')
+                } else {
+                    Registroconemail(state.email, state.contrasena);
 
-            alert('Se ha registrado correctamente')
-        }
-        }catch(error){
-            console.error(error);
-        }
+                    await addDoc(collection(db, 'Usuario'), {
+                        Apellidos: state.apellidos,
+                        Contrasena: state.contrasena,
+                        Email: state.email,
+                        FechaNacimiento: state.time,
+                        Nombre: state.nombre,
+                        NumeroTelefono: state.numerotelefono,
+                        tags: state.tags
+                    });
+
+                    alert('Se ha registrado correctamente')
+                }
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 
-    const compruebaEmail = async(email) =>{
-        
+    const compruebaEmail = async (email) => {
+
         const q = query(collection(db, "Usuario"), where("Email", "==", email));
         const querySnapshot = await getDocs(q);
         return querySnapshot.docs;
     }
 
-    const Registroconemail = (email,password) => {
+    const Registroconemail = (email, password) => {
         const auth = getAuth();
-        
+
         createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-          });
-        
-        };
-    async function RegisterUsersPrueba(){
-        return new Promise(async function(resolve,reject){
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege5@gmail.com',
-            contrasena:'S177013'
-        });
-        console.log(state.nombre)
-        resolve();
-    })
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+
     }
-    async function EliminarPrueba(usp){
-        return new Promise(async function(resolve,reject){
+
+    async function EliminarPrueba(usp) {
+        return new Promise(async function (resolve, reject) {
             const q = query(collection(db, "Usuario"), where("Nombre", "==", usp));
             const querySnapshot = await getDocs(q);
-                resolve(querySnapshot)
-            })
+            resolve(querySnapshot)
+        })
     }
+
     var arr2 = []
-    async function EliminarPrueba2(usp){
-        EliminarPrueba(usp).then((qu)=>{
-            return new Promise(async function(resolve,reject){
-            var arr = []
-            qu.forEach((doc) =>{
-                arr.push(doc.id)
+    async function EliminarPrueba2(usp) {
+        EliminarPrueba(usp).then((qu) => {
+            return new Promise(async function (resolve, reject) {
+                var arr = []
+                qu.forEach((doc) => {
+                    arr.push(doc.id)
+                })
+                arr2 = arr
+                resolve(arr)
+            })
         })
-        arr2 = arr
-        resolve(arr)
-        })
-     })
     }
-    async function EliminarPrueba3(usp){
-        EliminarPrueba2(usp).then((c)=>{
-            arr2.forEach((item) =>{
+
+    async function EliminarPrueba3(usp) {
+        EliminarPrueba2(usp).then((c) => {
+            arr2.forEach((item) => {
                 const a = doc(db, 'Usuario', item);
                 deleteDoc(a);
             })
@@ -126,214 +313,13 @@ const RegistrarUsuario1Screen = () => {
                 </Text>
             </View>
             <View style={screenStyles.button}>
-                <Button color='#177013'title="Probar:" onPress={() => RegisterUsersPrueba().then(() => {RegisterUser()})}/>
+                <Button color='#177013' title="Probar:" onPress={() => { ejecutarPrueba() }} />
             </View>
             <View style={screenStyles.button}>
-                <Button color='#177013'title="Eliminar Prueba:" onPress={() => EliminarPrueba3("Samuel")}/>
+                <Button color='#177013' title="Eliminar Prueba:" onPress={() => EliminarPrueba3("Samuel")} />
             </View>
         </ScrollView>
     )
 }
-
-/*
-		setState({
-            nombre:'',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege@gmail.com',
-            contrasena:'S177013'
-        }, () => {RegisterUser()});
-
-        setState({
-            nombre:'Samuel',
-            apellidos:'',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege1@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege2@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'',
-            email:'samuelvege3@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'',
-            contrasena:'S177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege4@gmail.com',
-            contrasena:''
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege5@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'123Sam123uel123',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege6@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser(); 
-        setState({
-            nombre:'Xx_Samuel_xX',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege7@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser(); 
-        setState({
-            nombre:'Samuel',
-            apellidos:'123Di123az123',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege8@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser(); 
-        setState({
-            nombre:'Samuel',
-            apellidos:'Xx_Diaz_xX',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege9@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser(); 
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'Esto626No',
-            email:'samuelvege10@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser(); 
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'123456789098765432177013',
-            email:'samuelvege11@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser(); 
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'177013',
-            email:'samuelvege12@gmail.com',
-            contrasena:'S177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege13@gmail.com',
-            contrasena:'177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege14@gmail.com',
-            contrasena:'samuel'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege15@gmail.com',
-            contrasena:'s177013'
-        });
-        RegisterUser();
-        setState({
-            nombre:'Samuel',
-            apellidos:'Diaz',
-            tags:'Deportista',
-            fechaNacimiento:'',
-            time: new Date("2020","02","22"),
-            numerotelefono:'626802895',
-            email:'samuelvege16@gmail.com',
-            contrasena:'S123'
-        });
-        RegisterUser();
-*/
 
 export default RegistrarUsuario1Screen
