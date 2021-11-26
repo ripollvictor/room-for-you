156dc9ac20252916c762c73e6a2b20d4d41227c9
Arturo <45513197+Calisto184AC0@users.noreply.github.com>
2021-11-26 17:10:02 +0100

    más cambios

--- tests/carga-imagenes/TestTime.js
+++ tests/carga-imagenes/TestTime.js
@@ -11,9 +11,14 @@ const TestTime = () => {
     }
 
     const testPNG = async () => {
-        console.time('Test PNG Small ~500KB')
-        await subirArchivo('./examples/prueba_PNG_S.png')
-        console.timeEnd('Test PNG Small ~500KB')
+        const t0 = performance.now()
+        await subirArchivo(require('./examples/prueba_PNG_S.png'))
+        const t1 = performance.now()
+
+        console.log((t1 - t0) / 1000 + ' s')
+        // console.time('Test PNG Small ~500KB')
+        // await subirArchivo('./examples/prueba_PNG_S.png')
+        // console.timeEnd('Test PNG Small ~500KB')
 
         // console.time('Test PNG Medium ~2MB')
         // await subirArchivo('./examples/prueba_PNG_M.png')
