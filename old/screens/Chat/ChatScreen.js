d9a11cf8a0718fe4cdc0820eb39558f385bfee1a
sergiaguilera <sergiaguileramorales@gmail.com>
2021-12-01 10:55:31 +0100

    falta poco query

--- old/screens/Chat/ChatScreen.js
+++ old/screens/Chat/ChatScreen.js
@@ -7,7 +7,7 @@ import Input from "../../components/Input/Input"
 import firebase from '../../database/firebase'
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { GiftedChat } from 'react-native-gifted-chat';
-import { getFirestore, collection, getDocs, doc ,addDoc, query,where,deleteDoc,setDoc,getDoc,updateDoc} from 'firebase/firestore/lite';
+import { getFirestore, collection, doc ,addDoc, getDocs,where,deleteDoc,setDoc,getDoc,updateDoc} from 'firebase/firestore/lite';
 
 
 const auth = getAuth();
@@ -17,20 +17,35 @@ const ChatScreen = ({ navigation }) => {
 
   const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: auth?.currentUser.email.toString(),
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
-  }, [])
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: auth?.currentUser.email.toString(),
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: 'React Native',
+  //         avatar: 'https://placeimg.com/140/140/any',
+  //       },
+  //     },
+  //   ])
+  // }, [])
+
+  useLayoutEffect(() => {
+    const q = query(collection(db, 'Mensajes'), orderBy('createdAt'))
+    const unsuscribe = db.getDocs('Mensajes').orderedBy('createdAt', 
+    'desc').onSapshot(snapshot=>setMessages(
+      snapshot.docs.map(doc=>({
+        _id:doc.data()._id,
+        text: doc.data().text,
+        createdAt: doc.data().createdAt.toDate(),
+        user: doc.data().user
+      }))
+      
+    ))
+    return unsuscribe;
+  })
 
   const onSend = useCallback(async(messages = []) => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
